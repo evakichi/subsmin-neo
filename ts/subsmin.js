46ceaa3e73a4d78920a8f74c953c108fa0b88748
Makoto ISHIHARA <evakichi@be.wakwak.com>
2025-02-07 22:19:35 +0900

    change.

--- ts/subsmin.js
+++ ts/subsmin.js
@@ -2,17 +2,83 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs = require("fs");
 var axios_1 = require("axios");
-function getStation() {
-    var inputToken = fs.readFileSync("./token.json", 'utf-8');
-    var tokenString = JSON.parse(inputToken);
+var inputToken = fs.readFileSync("/home/evakichi/token.json", 'utf-8');
+var tokenString = JSON.parse(inputToken);
+function getNextTrain(trainTimeTable, stationTimeTable, station, railway, trainInformation, railwayFare) {
+    console.log(trainTimeTable);
+    console.log(stationTimeTable);
+    console.log(station);
+    console.log(railway);
+    console.log(trainInformation);
+    console.log(railwayFare);
+}
+;
+function getRailwayFare(token, trainTimeTable, stationTimeTable, station, railway, trainInformation) {
+    axios_1.default.get("https://api.odpt.org/api/v4/odpt:RailwayFare", {
+        params: {
+            "odpt:operator": "odpt.Operator:MIR",
+            "acl:consumerKey": token,
+        }
+    })
+        .then(function (response) {
+        getNextTrain(trainTimeTable, stationTimeTable, station, railway, trainInformation, response.data);
+    })
+        .catch(function (error) {
+        console.error("Fail");
+        console.error(error);
+    })
+        .finally(function () {
+        //console.log("finish");
+    });
+}
+;
+function getTrainInformation(token, trainTimeTable, stationTimeTable, station, railway) {
+    axios_1.default.get("https://api.odpt.org/api/v4/odpt:TrainInformation", {
+        params: {
+            "odpt:operator": "odpt.Operator:MIR",
+            "acl:consumerKey": token,
+        }
+    })
+        .then(function (response) {
+        getRailwayFare(token, trainTimeTable, stationTimeTable, station, railway, response.data);
+    })
+        .catch(function (error) {
+        console.error("Fail");
+        console.error(error);
+    })
+        .finally(function () {
+        //console.log("finish");
+    });
+}
+;
+function getRailway(token, trainTimeTable, stationTimeTable, station) {
+    axios_1.default.get("https://api.odpt.org/api/v4/odpt:Railway", {
+        params: {
+            "odpt:operator": "odpt.Operator:MIR",
+            "acl:consumerKey": token,
+        }
+    })
+        .then(function (response) {
+        getTrainInformation(token, trainTimeTable, stationTimeTable, station, response.data);
+    })
+        .catch(function (error) {
+        console.error("Fail");
+        console.error(error);
+    })
+        .finally(function () {
+        //console.log("finish");
+    });
+}
+;
+function getStation(token, trainTimeTable, stationTimeTable) {
     axios_1.default.get("https://api.odpt.org/api/v4/odpt:Station", {
         params: {
             "odpt:operator": "odpt.Operator:MIR",
-            "acl:consumerKey": tokenString.token,
+            "acl:consumerKey": token,
         }
     })
         .then(function (response) {
-        console.log(response.data);
+        getRailway(token, trainTimeTable, stationTimeTable, response.data);
     })
         .catch(function (error) {
         console.error("Fail");
@@ -23,18 +89,15 @@ function getStation() {
     });
 }
 ;
-function getStationTimeTable() {
-    var inputToken = fs.readFileSync("./token.json", 'utf-8');
-    var tokenString = JSON.parse(inputToken);
+function getStationTimeTable(token, trainTimeTable) {
     axios_1.default.get("https://api.odpt.org/api/v4/odpt:StationTimetable", {
         params: {
             "odpt:operator": "odpt.Operator:MIR",
-            "acl:consumerKey": tokenString.token,
+            "acl:consumerKey": token,
         }
     })
         .then(function (response) {
-        console.log(response.data);
-        getStation();
+        getStation(token, trainTimeTable, response.data);
     })
         .catch(function (error) {
         console.error("Fail");
@@ -45,18 +108,15 @@ function getStationTimeTable() {
     });
 }
 ;
-function getTrainTimeTable() {
-    var inputToken = fs.readFileSync("./token.json", 'utf-8');
-    var tokenString = JSON.parse(inputToken);
+function getTrainTimeTable(token) {
     axios_1.default.get("https://api.odpt.org/api/v4/odpt:TrainTimetable", {
         params: {
             "odpt:operator": "odpt.Operator:MIR",
-            "acl:consumerKey": tokenString.token,
+            "acl:consumerKey": token,
         }
     })
         .then(function (response) {
-        console.log(response.data);
-        getStationTimeTable();
+        getStationTimeTable(token, response.data);
     })
         .catch(function (error) {
         console.error("Fail");
@@ -67,4 +127,4 @@ function getTrainTimeTable() {
     });
 }
 ;
-getTrainTimeTable();
+getTrainTimeTable(tokenString.token);
--- ts/subsmin.ts
+++ ts/subsmin.ts
@@ -1,22 +1,93 @@
 import * as fs from "fs";
 import * as yargs from "yargs";
 import axis from 'axios';
+	
 
 type TokenString={
 	token:string;
-}
-function getStation(){
-	const inputToken:string = fs.readFileSync("./token.json",'utf-8');
-	const tokenString:TokenString = JSON.parse(inputToken);
+};
+
+const inputToken:string = fs.readFileSync("/home/evakichi/token.json",'utf-8');
+const tokenString:TokenString = JSON.parse(inputToken);
+function getNextTrain(trainTimeTable:any,stationTimeTable:any,station:any,railway:any,trainInformation:any,railwayFare:any){
+	 console.log(trainTimeTable);
+	 console.log(stationTimeTable);
+	 console.log(station);
+	 console.log(railway);
+	 console.log(trainInformation);
+	 console.log(railwayFare);
+};
+function getRailwayFare(token:string,trainTimeTable:any,stationTimeTable:any,station:any,railway:any,trainInformation:any){
+	axis.get("https://api.odpt.org/api/v4/odpt:RailwayFare",
+		 {
+			 params: {
+		 		"odpt:operator": "odpt.Operator:MIR",
+				"acl:consumerKey": token,
+			 }
+		 })
+		 .then((response)=>{
+			 getNextTrain(trainTimeTable,stationTimeTable,station,railway,trainInformation,response.data);
+		 }
+		 )
+		 .catch((error) =>{
+			 console.error("Fail");
+			 console.error(error);
+		 })
+		 .finally(() => {
+			 //console.log("finish");
+		 });
+};
+function getTrainInformation(token:string,trainTimeTable:any,stationTimeTable:any,station:any,railway:any){
+	axis.get("https://api.odpt.org/api/v4/odpt:TrainInformation",
+		 {
+			 params: {
+		 		"odpt:operator": "odpt.Operator:MIR",
+				"acl:consumerKey": token,
+			 }
+		 })
+		 .then((response)=>{
+			 getRailwayFare(token,trainTimeTable,stationTimeTable,station,railway,response.data);
+		 }
+		 )
+		 .catch((error) =>{
+			 console.error("Fail");
+			 console.error(error);
+		 })
+		 .finally(() => {
+			 //console.log("finish");
+		 });
+};
+function getRailway(token:string,trainTimeTable:any,stationTimeTable:any,station:any){
+	axis.get("https://api.odpt.org/api/v4/odpt:Railway",
+		 {
+			 params: {
+		 		"odpt:operator": "odpt.Operator:MIR",
+				"acl:consumerKey": token,
+			 }
+		 })
+		 .then((response)=>{
+			 getTrainInformation(token,trainTimeTable,stationTimeTable,station,response.data);
+		 }
+		 )
+		 .catch((error) =>{
+			 console.error("Fail");
+			 console.error(error);
+		 })
+		 .finally(() => {
+			 //console.log("finish");
+		 });
+};
+
+function getStation(token:string,trainTimeTable:any,stationTimeTable:any){
 	axis.get("https://api.odpt.org/api/v4/odpt:Station",
 		 {
 			 params: {
 		 		"odpt:operator": "odpt.Operator:MIR",
-				"acl:consumerKey": tokenString.token,
+				"acl:consumerKey": token,
 			 }
 		 })
 		 .then((response)=>{
-			 console.log(response.data);
+			 getRailway(token,trainTimeTable,stationTimeTable,response.data);
 		 }
 		 )
 		 .catch((error) =>{
@@ -28,19 +99,16 @@ function getStation(){
 		 });
 };
 
-function getStationTimeTable(){
-	const inputToken:string = fs.readFileSync("./token.json",'utf-8');
-	const tokenString:TokenString = JSON.parse(inputToken);
+function getStationTimeTable(token:string,trainTimeTable:any){
 	axis.get("https://api.odpt.org/api/v4/odpt:StationTimetable",
 		 {
 			 params: {
 		 		"odpt:operator": "odpt.Operator:MIR",
-				"acl:consumerKey": tokenString.token,
+				"acl:consumerKey": token,
 			 }
 		 })
 		 .then((response)=>{
-			 console.log(response.data);
-			 getStation(); 
+			 getStation(token,trainTimeTable,response.data); 
 		 }
 		 )
 		 .catch((error) =>{
@@ -52,19 +120,16 @@ function getStationTimeTable(){
 		 });
 };
 
-function getTrainTimeTable(){
-	const inputToken:string = fs.readFileSync("./token.json",'utf-8');
-	const tokenString:TokenString = JSON.parse(inputToken);
+function getTrainTimeTable(token:string){
 	axis.get("https://api.odpt.org/api/v4/odpt:TrainTimetable",
 		 {
 			 params: {
 		 		"odpt:operator": "odpt.Operator:MIR",
-				"acl:consumerKey": tokenString.token,
+				"acl:consumerKey": token,
 			 }
 		 })
 		 .then((response)=>{
-			 console.log(response.data);
-			 getStationTimeTable(); 
+			 getStationTimeTable(token,response.data); 
 		 }
 		 )
 		 .catch((error) =>{
@@ -76,4 +141,4 @@ function getTrainTimeTable(){
 		 });
 };
 
-getTrainTimeTable();
+getTrainTimeTable(tokenString.token);
